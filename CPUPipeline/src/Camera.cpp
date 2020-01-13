7af18a90e65e853ae8e3f33b8631589359c81ad6
Michał Milewski <miles98@interia.pl>
2020-01-14 00:20:07 +0100

    z-buffer works now here

--- CPUPipeline/src/Camera.cpp
+++ CPUPipeline/src/Camera.cpp
@@ -22,12 +22,12 @@ void Camera::SetPerspective(float fov_y, float aspect, float near, float far)
 {
 	glm::mat4 persp = glm::mat4(1.0f);
 	float e = 1 / tan(fov_y / 2);
-	persp[0][0] = e* aspect;
-	persp[1][1] = e ;
-	persp[2][2] = ((far + near) / (far - near));
+	persp[0][0] = e;
+	persp[1][1] = e / aspect;
+	persp[2][2] = -((far + near) / (far - near));
 	persp[3][2] = -((far * near * 2) / (far - near));
 	persp[3][3] = 0;
-	persp[2][3] = 1;
+	persp[2][3] = -1;
 	m_projection_matrix = persp;
 
 }
@@ -53,28 +53,23 @@ void Camera::LookAt(glm::vec3 pos, glm::vec3 front, glm::vec3 up)
 	this->up = up;
 
 	glm::vec3 const f(glm::normalize(front));
-	glm::vec3 const r(glm::normalize(cross(up, f)));
-	glm::vec3 const u(glm::cross(f, r));
+	glm::vec3 const s(glm::normalize(cross(f, up)));
+	glm::vec3 const u(glm::cross(s, f));
 
 	glm::mat4 Result(1.0f);
-	Result[0][0] = r.x;
-	Result[1][0] = r.y;
-	Result[2][0] = r.z;
+	Result[0][0] = s.x;
+	Result[1][0] = s.y;
+	Result[2][0] = s.z;
 	Result[0][1] = u.x;
 	Result[1][1] = u.y;
 	Result[2][1] = u.z;
-	Result[0][2] = f.x;
-	Result[1][2] = f.y;
-	Result[2][2] = f.z;
-	//Result[3][0] = -glm::dot(r, pos);
-	//Result[3][1] = -glm::dot(u, pos);
-	//Result[3][2] = glm::dot(f, pos);
-
-	glm::mat4 sec(1.0f);
-	sec[3][0] = - pos.x;
-	sec[3][1] = - pos.y;
-	sec[3][2] = - pos.z;
-	m_world_matrix = Result* sec;
+	Result[0][2] = -f.x;
+	Result[1][2] = -f.y;
+	Result[2][2] = -f.z;
+	Result[3][0] = -glm::dot(s, pos);
+	Result[3][1] = -glm::dot(u, pos);
+	Result[3][2] = glm::dot(f, pos);
+	m_world_matrix = Result;
 	//m_world_matrix = glm::lookAt(pos, pos + front, up);
 }
 
